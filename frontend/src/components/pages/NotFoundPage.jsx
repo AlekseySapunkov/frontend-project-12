c65e013094254086ca50267d7bcc88d0649b9a45
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2023-03-13 15:48:48 +0300

    Create NotFoundPage.jsx

---
+++ frontend/src/components/pages/NotFoundPage.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Image } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import Header from '../Header';
+import image from '../../assets/NotFoundImg.svg';
+
+const NotFound = () => {
+  const { t } = useTranslation();
+  return (
+    <div className="d-flex flex-column h-100">
+      <Header />
+      <div className="text-center">
+        <Image className="h-25" fluid src={image} alt={t('notFound.title')} />
+        <h1 className="h4 text-muted">{t('notFound.title')}</h1>
+        <p className="text-muted">
+          {t('notFound.footerFirst')}
+          <Link to="/">{t('notFound.footerSecond')}</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
