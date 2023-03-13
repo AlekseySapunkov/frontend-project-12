0397499ac88df9e43c3e6ab0a95d49febc5b6f10
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2023-03-13 15:50:34 +0300

    Create App.jsx

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react';
+import {
+  BrowserRouter, Routes, Route, Navigate, Outlet,
+} from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import LoginPage from './pages/LoginPage';
+import MainPage from './pages/MainPage';
+import RegisterPage from './pages/RegisterPage';
+import NotFoundPage from './pages/NotFoundPage';
+import socket from '../socket';
+import { actions as channelsActions } from '../slices/channelsSlice';
+import { actions as messagesActions } from '../slices/messagesSlice';
+
+const PrivateRoute = () => {
+  const isAuth = localStorage.getItem('userId');
+  return (
+    isAuth ? <Outlet /> : <Navigate to="login" />
+  );
+};
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    socket.on('newMessage', (payload) => {
+      dispatch(messagesActions.addMessage(payload));
+    });
+    socket.on('newChannel', (payload) => {
+      dispatch(channelsActions.addChannel(payload));
+    });
+    socket.on('renameChannel', (payload) => {
+      dispatch(channelsActions.renameChannel(payload));
+    });
+    socket.on('removeChannel', (payload) => {
+      dispatch(channelsActions.removeChannel(payload));
+    });
+  }, [dispatch]);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<MainPage />} />
+        </Route>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<RegisterPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
